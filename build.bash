13dd9c4415a696bd253f109c98d2450f499b2f1c
SupremeT <1012143610@qq.com>
2018-05-27 03:08:45 +0800

    try build

--- build.bash
+++ build.bash
@@ -1,5 +1,1 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-mkdir -p bin
-find ./src/Code -name *.java | javac -d bin -cp "./lib/*" @/dev/stdin
+set -e; cd "$(dirname "$0")"; mkdir -p bin; find ./src/Code -name *.java | javac -d bin -cp "./lib/*" @/dev/stdin;
