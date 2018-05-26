ca76619c982c8714fc9b52f3c9459dad794ddf6f
SupremeT <1012143610@qq.com>
2018-05-27 00:10:14 +0800

    fix build

--- build.bash
+++ build.bash
@@ -1,8 +1,5 @@
-#!/bin/bash
-# this script is called when the judge is building your compiler.
-# no argument will be passed in.
-
+#!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src/Code-name *.java | javac -d bin -cp "./lib/*" @/dev/stdin
+find ./src/Code -name *.java | javac -d bin -cp "./lib/*" @/dev/stdin
